28c0a75cfa5c728e89a76ed88ab83849d7ffc5d1
Ninh Nguyen (阮安宁) <ninhnguyenweb@gmail.com>
2021-12-05 19:54:14 -0800

    Added 2 more Python Files

---
+++ Program_Exercise/Car_MPG.py
@@ -0,0 +1,6 @@
+# This program calculates a car's miles-per-Gallon (MPG).
+
+Milesdriven = float(input('Enter the amount of miles driven: '))
+Gallonsofgasused = float(input('Enter the amount of gallons: '))
+mpg = Milesdriven / Gallonsofgasused
+print(mpg)
