faf09f5ed3f430de83e46688f4caa085c6d7fdf2
Ninh Nguyen (阮安宁) <ninhnguyenweb@gmail.com>
2021-12-05 17:24:35 -0800

    Delete Program_Exercise directory

--- Program_Exercise/Land_Size.py
+++
@@ -1,7 +0,0 @@
-# 3. This program calculates the number of acres in a tract
-
-acre = 43560
-square_feet_input = input('Enter the total square feet in a land: ')
-square_feet = float(square_feet_input)
-total_acre = square_feet / acre
-print(f'{total_acre:,.2f}')
--- Program_Exercise/Personal_Info.py
+++
@@ -1,6 +0,0 @@
-# This program displays the following personal information after user input.
-
-name = input('Enter your name: ')
-address = input('Enter your address: ')
-telephone_number = input('Enter your number: ')
-college_major = input('Enter your major: ')
