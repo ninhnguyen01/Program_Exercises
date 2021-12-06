204494e772e04d1d0307a3e9ebbcbafeff2c13f0
Ninh Nguyen (阮安宁) <ninhnguyenweb@gmail.com>
2021-12-05 21:16:02 -0800

    Delete Grapevines.py

--- Program_Exercise/Grapevines.py
+++
@@ -1,8 +0,0 @@
-# This program makes a calculation for a vineyard owner
-# for planting the number of grapevines in a row.
-
-R = int(input('Enter the length of row, in feet: '))
-E = int(input('Enter the amount of space, in feet: '))
-S = int(input('Enter the amount of space between the vines, in feet: '))
-
-V = (R - 2(E)) // S
