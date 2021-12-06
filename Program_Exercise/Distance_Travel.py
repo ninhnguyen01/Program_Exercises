03e9dc76a9bbf944778189d26d675626df4fcd09
Ninh Nguyen (阮安宁) <ninhnguyenweb@gmail.com>
2021-12-05 17:22:42 -0800

    Added Python files via upload
    4 files have been uploaded.

---
+++ Program_Exercise/Distance_Travel.py
@@ -0,0 +1,6 @@
+# This program shows the distance a car travels down the interstate with no accidents or delays.
+
+speed = 70          # speed in miles per hour
+time = int(input('Enter the time in hours (6,10,or 15): '))
+distance = speed * time
+print(distance)
