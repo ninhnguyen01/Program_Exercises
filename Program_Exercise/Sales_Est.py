92db3bcb30f7cb933711c875a560584c68449308
Ninh Nguyen (阮安宁) <ninhnguyenweb@gmail.com>
2021-12-05 17:28:10 -0800

    Added Python Files
    1st set of 4 files.

---
+++ Program_Exercise/Sales_Est.py
@@ -0,0 +1,7 @@
+# This program demonstrates sales prediction.
+
+# annual profit = 23% of total sales
+total_sales_input = input('Enter the projected amount of total sales: ')
+total_sales = float(total_sales_input)
+profit = total_sales * 0.23
+print(profit)
