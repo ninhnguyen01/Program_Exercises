11404acdb3b2411a37f691c496666e19e3cccece
Ninh Nguyen (阮安宁) <ninhnguyenweb@gmail.com>
2021-12-07 00:12:28 -0800

    Delete Ocean_Lvl.py

--- Ocean_Lvl.py
+++
@@ -1,9 +0,0 @@
-# This program displays the number of millimeters that the ocean 
-# will have risen each year for the next 25 years.
-
-ocean_lvl = 1.6 
-total = 0
-
-for o in range(1,26):
-    o *= ocean_lvl
-    print(f'{o:.1f}')
